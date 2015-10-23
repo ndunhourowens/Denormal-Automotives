143fede70a9815b898045de1e9f14570a64bf7a2
ndunhourowens <ndunhour@hotmail.com>
2015-10-22 19:11:19 -1000

    create some queries to the car_models database

---
+++ denormal_cars.sql
@@ -0,0 +1,4 @@
+SELECT DISTINCT make_title FROM car_models;
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'VOLKS';
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+SELECT * FROM car_models WHERE year > 2009 AND year < 2016;
